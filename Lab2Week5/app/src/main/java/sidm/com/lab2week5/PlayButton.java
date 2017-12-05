ad57dc610eb989a6864ae188c5ebcbc15f0a797b
SIDM\160324T <jerrywong243@gmail.com>
2017-12-05 20:07:59 +0800

    Button class and play button class example

---
+++ Lab2Week5/app/src/main/java/sidm/com/lab2week5/PlayButton.java
@@ -0,0 +1,25 @@
+package sidm.com.lab2week5;
+
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+
+public class PlayButton extends Button
+{
+    @Override
+    public void Init(SurfaceView _view)
+    {
+
+    }
+
+    @Override
+    public void Update(float dt)
+    {
+
+    }
+
+    @Override
+    public void Render(Canvas _canvas)
+    {
+
+    }
+}
